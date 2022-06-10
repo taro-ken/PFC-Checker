360a6a8ff7a434126dae81f9755403eac7c9d7f0
kentaro kimoto <exiler2411k@gmail.com>
2022-06-10 20:20:55 +0900

    init

---
+++ PFC-Checker/PFC-Checker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PFC-Checker
+//
+//  Created by 木元健太郎 on 2022/06/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
